666a037f82b8d136b3d60279394c5c4fc852f8bd
Lenovo <Lenovo@Lenovo-PC>
2017-01-02 23:50:53 +0530

    Login of user

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -5,32 +5,65 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import entity.User;
 import entity.UserRole;
+import entity.UserWrapper;
 import repository.UserRoleRepository;
 import service.UserService;
 
 @Controller
+@SessionAttributes(value = "user_info", types = { UserWrapper.class })
 public class HomeController {
 	@Autowired
 	UserService service;
-	@RequestMapping(value = {"r","register"},method=RequestMethod.GET)
+
+	@RequestMapping(value = { "r", "register" }, method = RequestMethod.GET)
 	public String register(@ModelAttribute("reg_user") User u) {
 		return "register";
 	}
-	
-	@RequestMapping(value = {"r","register"},method=RequestMethod.POST)
+
+	@RequestMapping(value = { "r", "register" }, method = RequestMethod.POST)
 	public String registerUser(@ModelAttribute("reg_user") User u) {
 		service.registerUser(u);
 		return "redirect:/r";
 	}
-	
-	
-	@RequestMapping(value = "l")
-	public String login(){
-		UserRole r=service.getUserRoleByName("ROLE_ADMIN");
-		System.out.println(r.getUser_role_id());
+
+	@ModelAttribute("user_info")
+	private UserWrapper createSession() {
+		return new UserWrapper();
+	}
+
+	@RequestMapping(value = "login")
+	public String login(@ModelAttribute("user_info") UserWrapper user) {
 		return "login";
+	}
+
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	public String authentication(@ModelAttribute("user_info") UserWrapper user,
+			RedirectAttributes attr, SessionStatus session) {
+		User u = service.authorize(user);
+		
+		if (u == null) {
+			attr.addFlashAttribute("login_status", "Invalid username/password.");
+			session.setComplete();
+			return "redirect:/login";
+		}
+		System.out.println("User " + u.getEmail());
+		user.setRole(u.getRole().getRole());
+		return "redirect:/welcome";
+	}
+
+	@RequestMapping("welcome")
+	public String welcomePage() {
+		return "user/userhome";
+	}
+	@RequestMapping("logout")
+	public String logout(SessionStatus session) {
+		session.setComplete();
+		return "redirect:/login";
 	}
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -7,25 +7,35 @@ import repository.UserRepository;
 import repository.UserRoleRepository;
 import entity.User;
 import entity.UserRole;
+import entity.UserWrapper;
+
 @Service
 public class UserService {
 
 	@Autowired
 	UserRoleRepository userRoleRepo;
-	
+
 	@Autowired
 	UserRepository userRepo;
-	
-	public UserRole getUserRoleByName(String name){
+
+	public UserRole getUserRoleByName(String name) {
 		return userRoleRepo.findByRole(name);
 	}
-	
-	public boolean registerUser(User user){
+
+	public boolean registerUser(User user) {
 		user.setEnabled("inactive");
 		user.setRole(getUserRoleByName("ROLE_USER"));
-		User u=userRepo.save(user);
-		if(u!=null)
+		User u = userRepo.save(user);
+		if (u != null)
 			return true;
 		return false;
+	}
+
+	public User authorize(UserWrapper user) {
+		User u = userRepo.getByUsernameAndPassword(user.getUsername(),
+				user.getPassword());
+		if (u != null)
+			return u;
+		return null;
 	}
 }
